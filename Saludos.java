ce8c77e1a1ba9807a3c1edf82014cff81e33308c
JuanMarin17 <juandavidmr77@gmail.com>
2026-02-19 21:02:42 -0500

    AGREGO metodo hablar

--- Saludos.java
+++ Saludos.java
@@ -3,4 +3,8 @@ public class Saludos{
     public static void main(String[] args) {
         System.out.println("Hola German");
     }
+
+    public static void hablar(){
+        System.out.println("Momento de hablar");
+    }
 }
