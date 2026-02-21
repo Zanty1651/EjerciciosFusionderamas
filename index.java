4e9f4773c2c0a9dcd6b3adc2593da4851dd5345a
Zanty1651 <santiloaiza2000@gmail.com>
2026-02-20 20:11:23 -0500

    AGREGADO metodo saludar

--- index.java
+++ index.java
@@ -3,4 +3,7 @@ public class index{
     public static void main(String[] args) {
         System.out.println("Hola mundillo");
     }
+    public static void saludar(){
+        System.out.println("Momento de saludar");
+    }
 }
